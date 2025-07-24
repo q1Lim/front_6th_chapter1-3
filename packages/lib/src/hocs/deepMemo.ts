21af6a3f53dfa7cea0c76d459154af60efb799ca
q1Lim <cochlea00@gmail.com>
2025-07-24 18:27:39 +0900

    feat: HOC memo, deepMemo 작성

--- packages/lib/src/hocs/deepMemo.ts
+++ packages/lib/src/hocs/deepMemo.ts
@@ -1,5 +1,10 @@
 import type { FunctionComponent } from "react";
+import { memo } from "./memo.ts";
+import { deepEquals } from "../equals";
 
+// deepMemo HOC는 컴포넌트의 props를 깊은 비교하여 불필요한 리렌더링을 방지
 export function deepMemo<P extends object>(Component: FunctionComponent<P>) {
-  return Component;
+  // deepEquals 함수를 사용하여 props 비교
+  // 앞에서 만든 memo도 활용
+  return memo(Component, deepEquals);
 }
--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,22 @@
-import { type FunctionComponent } from "react";
+import { type FunctionComponent, type ReactNode } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
+// memo HOC는 컴포넌트의 props를 얕은 비교하여 불필요한 렌더링을 방지
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  // 1. 이전 props를 저장할 ref 생성
+  // 2. 메모이제이션된 컴포넌트 생성
+  // 3. equals 함수를 사용하여 props 비교
+  // 4. props가 변경된 경우에만 새로운 렌더링 수행
+
+  return (props: P) => {
+    const prevPropsRef = useRef<P | null>(null);
+    const renderOutputRef = useRef<ReactNode | null>(null);
+
+    if (!equals(prevPropsRef.current, props)) {
+      prevPropsRef.current = props;
+      renderOutputRef.current = Component(props) as ReactNode;
+    }
+    return renderOutputRef.current;
+  };
 }
